ac86abd8d4dae780a8156b7bfa5d395e1e1cc50f
skywalkerwin <47264358+skywalkerwin@users.noreply.github.com>
2019-05-03 23:57:33 -0600

    Add files via upload
    Trying to understand class hierarchy within react and how to work with states better

--- src/App.js
+++ src/App.js
@@ -18,23 +18,142 @@ import {
 import classnames from "classnames";
 import { updateExpression } from "@babel/types";
 
-class App extends Component {
-  state = {
-    plan: {
-      t1: [
-        { id: 1, content: "Buy some milk!" },
-        { id: 2, content: "Work more..." }
-      ],
-      t2: [
-        { id: 1, content: "GO TO STORE!" },
-        { id: 2, content: "PLAY GAMES..." }
-      ]
-    },
-    currentTodo: null,
-    activeTab: "t1",
-    tabid: "todo1",
-    tabname: "Todo's 1"
+class Bit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: props[0],
+      id: props[1]
+    };
+  }
+}
+
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bits: {},
+      numBits: 0,
+      taskName: props[0]
+    };
+  }
+  addBit(bit) {
+    let b = new Bit([bit, Math.random()]);
+    const newBits = { ...this.bits, b };
+    this.setState({
+      bits: newBits,
+      numBits: this.numBits + 1
+    });
+  }
+  delBit(bit) {
+    const bitid = bit.id;
+    const newBits = this.bits.filter(curBit => {
+      return curBit.id != bitid;
+    });
+    this.setState({
+      bits: newBits,
+      numBits: this.newBits - 1
+    });
+  }
+}
+
+class Plan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: {},
+      taskNames: [],
+      numTasks: 0,
+      currentTask: " "
+    };
+  }
+
+  addTask(taskName) {
+    let t = new Task(taskName);
+    const newTasks = { ...this.tasks, t };
+    const newTaskNames = [...this.taskNames, taskName];
+    this.setState({
+      tasks: newTasks,
+      taskNames: newTaskNames,
+      numTasks: this.numTasks + 1
+    });
+  }
+  delTask(taskName) {
+    const t = new Task(taskName);
+    const newTasks = { ...this.tasks, t };
+    const newTaskNames = [...this.taskNames, taskName];
+    this.setState({
+      tasks: newTasks,
+      taskNames: newTaskNames,
+      numTasks: this.numTasks + 1
+    });
+  }
+  toggle(tab) {
+    if (this.state.currentTask !== tab) {
+      this.setState({
+        currentTask: tab
+      });
+    }
+  }
+  createTabs = () => {
+    let table = [];
+    for (let i = 0; i < this.state.numTasks; i++) {
+      const t = (
+        <NavItem>
+          {" "}
+          <NavLink
+            // className={classnames({ active: this.state.activeTab === 1 })}
+            onClick={() => {
+              this.toggle(this.state.taskNames[i]);
+            }}
+          >
+            {this.state.taskNames[i]}
+          </NavLink>
+        </NavItem>
+      );
+      table.push(t);
+    }
+    return <Nav tabs> {table} </Nav>;
   };
+  render() {
+    return (
+      <div className="todo-app container">
+        {this.createTabs()}
+        <h1 className="blue-text"> {this.state.tabname}</h1>
+        <Todos
+          className="bottom"
+          todos={this.state.plan[this.state.activeTab]}
+          deleteTodo={this.deleteTodo}
+        />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    let t = new Plan(1);
+    this.state = {
+      plan: {
+        t1: [
+          { id: 1, content: "Buy some milk!" },
+          { id: 2, content: "Work more..." }
+        ],
+        t2: [
+          { id: 1, content: "GO TO STORE!" },
+          { id: 2, content: "PLAY GAMES..." }
+        ]
+      },
+      currentTodo: null,
+      activeTab: "t1",
+      tabid: "todo1",
+      tabname: "Todo's 1",
+      numtabs: 1,
+      test: t
+    };
+  }
   toggle(tab) {
     var tabstr = "Todo's ";
     if (this.state.activeTab !== tab) {
@@ -45,22 +164,18 @@ class App extends Component {
     }
   }
   deleteTodo = id => {
-    console.log(this.state.plan[this.state.activeTab]);
-    console.log(this.state.activeTab);
+    // console.log(this.state.plan[this.state.activeTab]);
+    // console.log(this.state.activeTab);
     const todos = this.state.plan[this.state.activeTab].filter(todo => {
       return todo.id != id;
     });
-    // let newplan = Object.assign({}, this.state.plan);
-    // newplan[this.state.activeTab] = todos;
-    // this.setState({
-    //   plan: newplan
-    // });
     const newplan = { ...this.state.plan };
     newplan[this.state.activeTab] = todos;
     this.setState({
       plan: newplan
     });
   };
+
   addTodo = todo => {
     todo.id = Math.random();
     const newtodo = [...this.state.plan[this.state.activeTab], todo];
@@ -79,7 +194,7 @@ class App extends Component {
           <NavItem>
             {" "}
             <NavLink
-              className={classnames({ active: this.state.activeTab === 1 })}
+              // className={classnames({ active: this.state.activeTab === 0 })}
               onClick={() => {
                 this.toggle(1);
               }}
@@ -90,7 +205,7 @@ class App extends Component {
           <NavItem>
             {" "}
             <NavLink
-              className={classnames({ active: this.state.activeTab === 2 })}
+              // className={classnames({ active: this.state.activeTab === 0 })}
               onClick={() => {
                 this.toggle(2);
               }}
@@ -99,8 +214,9 @@ class App extends Component {
             </NavLink>
           </NavItem>
         </Nav>
-        <h1 className="center blue-text"> {this.state.tabname}</h1>
+        <h1 className="blue-text"> {this.state.tabname}</h1>
         <Todos
+          className="bottom"
           todos={this.state.plan[this.state.activeTab]}
           deleteTodo={this.deleteTodo}
         />
---
+++ src/Plan.js
@@ -0,0 +1,161 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+// import update from "immutability-helper";
+import {
+  // TabContent,
+  // TabPane,
+  Nav,
+  NavItem,
+  NavLink
+  // Card,
+  // Button,
+  // CardTitle,
+  // CardText,
+  // Row,
+  // Col
+} from "reactstrap";
+import classnames from "classnames";
+import { updateExpression } from "@babel/types";
+
+class Bit extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props);
+    this.state = {
+      content: props[0],
+      id: props[1]
+    };
+  }
+}
+
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bits: {},
+      numBits: 0,
+      taskName: props[0]
+    };
+  }
+  addBit(bit) {
+    const newBits = { ...this.bits };
+    newBits[this.numBits + 1] = bit;
+    this.setState({
+      bits: newBits,
+      numBits: this.numBits + 1
+    });
+  }
+  delBit(bit) {
+    const bitid = bit.state.id;
+    const newBits = this.bits.filter(curBit => {
+      return curBit.state.id != bitid;
+    });
+    this.setState({
+      bits: newBits,
+      numBits: this.newBits - 1
+    });
+  }
+}
+
+class Plan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: {},
+      taskNames: [],
+      numTasks: 0,
+      currentTask: ""
+    };
+  }
+  componentDidMount() {
+    // const bit1 = new Bit(["Design your plan", 1]);
+    // const bit1 = new Bit(["Design your plan", 1]);
+    // const task1 = new Task(["Task 1"]);
+    // this.addTask(task1);
+    // this.state.tasks[task1.state.taskName].addBit(bit1);
+    // console.log("TEST");
+    // // console.log(this.state.currentTask);
+    // // console.log(this.state.tasks[this.state.currentTask]);
+    // console.log("TEST");
+  }
+  check() {
+    console.log(this.state.taskNames);
+    console.log(this.state.tasks);
+    console.log(this.state.currentTask);
+  }
+  addTask(task) {
+    const tname = task.state.taskName;
+    const newTasks = { ...this.tasks };
+    newTasks[tname] = task;
+    var names;
+    if (this.state.numTasks == 0) {
+      names = [tname];
+      this.setState({
+        currentTask: tname
+      });
+    } else {
+      names = [this.taskNames, tname];
+    }
+    // const newTaskNames = [this.taskNames, tname];
+    this.setState({
+      tasks: newTasks,
+      taskNames: names,
+      numTasks: this.numTasks + 1
+    });
+  }
+  delTask(taskName) {
+    const t = new Task(taskName);
+    const newTasks = { ...this.tasks, t };
+    const newTaskNames = [...this.taskNames, taskName];
+    this.setState({
+      tasks: newTasks,
+      taskNames: newTaskNames,
+      numTasks: this.numTasks + 1
+    });
+  }
+  toggle(tab) {
+    if (this.state.currentTask !== tab) {
+      this.setState({
+        currentTask: tab
+      });
+    }
+  }
+  createTabs = () => {
+    let table = [];
+    for (let i = 0; i < this.state.numTasks; i++) {
+      const t = (
+        <NavItem>
+          {" "}
+          <NavLink
+            // className={classnames({ active: this.state.activeTab === 1 })}
+            onClick={() => {
+              this.toggle(this.state.taskNames[i]);
+            }}
+          >
+            {this.state.taskNames[i]}
+          </NavLink>
+        </NavItem>
+      );
+      table.push(t);
+    }
+    return <Nav tabs> {table} </Nav>;
+  };
+  render() {
+    this.check();
+    return (
+      <div className="todo-app container">
+        {this.createTabs()}
+        <h1 className="blue-text"> {this.state.tabname}</h1>
+        {/* <Todos
+          className="bottom"
+          todos={this.state.plan[this.state.activeTab]}
+          deleteTodo={this.deleteTodo}
+        />
+        <AddTodo addTodo={this.addTodo} /> */}
+      </div>
+    );
+  }
+}
+
+export default Plan;
