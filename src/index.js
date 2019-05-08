f1dde4a2bd3d43ef407758b5b20c8f271cf36a43
skywalkerwin <47264358+skywalkerwin@users.noreply.github.com>
2019-05-07 21:10:06 -0600

    Add files via upload
    reverted component state approach where I try to localize task data within each component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import { connect } from "react-redux";
-// import App from "./App";
+import App from "./App";
 import Plan from "./Plan";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -18,7 +18,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //   }
 // }
 // ReactDOM.render(<App />, document.getElementById("headertabs"));
-ReactDOM.render(<Plan />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(<App />, document.getElementById("root2"));
 
 // If you want your app to work offline and load faster, you can change
